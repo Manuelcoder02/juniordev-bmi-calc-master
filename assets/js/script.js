3ca792057eb8dc470f48d3b3f49df0a9088cb8f8
adefenwa <sotundeemmanuel@gmail.com>
2024-05-18 13:52:03 +0100

    updated commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ console.log(formEL);
 inputWeightMetric.addEventListener('mouseleave', function(e){
   e.preventDefault();
   const height = Number(inputHeightMetric.value);
-      const weight = Number(inputWeightMetric.value);
+  const weight = Number(inputWeightMetric.value);
   const bmi = Number.parseFloat(weight / (height * height));
       console.log(bmi);;
 })
